7bc3a72e773d2de8a139737a1efebcd7b6d5d85d
ryanluc7reis <ryan.dev18@outlook.com>
2024-09-07 01:18:18 -0300

    Criado rota verify-session

---
+++ src/controllers/user/validateSession.ts
@@ -0,0 +1,26 @@
+import { Router } from "express";
+
+import { verifyToken } from "../../../utils/auth";
+
+const sessionVerification = Router();
+
+sessionVerification.get("/verify-session", verifyToken, async (req, res) => {
+  try {
+    const session = {
+      user: req.user,
+      userId: req.userId,
+      email: req.email,
+      fullName: req.fullName,
+    };
+    if (session) {
+      return res.status(200).send(session);
+    }
+
+    return res.status(401).send({ error: "Sessão inválida" });
+  } catch (error) {
+    console.error("Erro ao validar sessão:", error);
+    res.status(500).json({ message: "Erro no servidor" });
+  }
+});
+
+export default sessionVerification;
