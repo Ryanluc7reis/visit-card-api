0781b15f4bab6352016c89fd45539136d2815f59
ryanluc7reis <ryan.dev18@outlook.com>
2024-09-11 03:47:18 -0300

    Adicionando detalhes de erro em validation

--- lib/middlewares/validation.ts
+++ lib/middlewares/validation.ts
@@ -4,11 +4,11 @@ import { ObjectSchema } from "joi";
 export default function validation(schema: ObjectSchema) {
   return (req: Request, res: Response, next: NextFunction) => {
     const { error } = schema.validate(req.body, { abortEarly: false });
-
     if (error) {
       const errorDetails = error.details.map((detail) => ({
         field: detail.path.join("."),
         message: detail.message,
+        type: detail.type,
       }));
 
       return res.status(400).json({
