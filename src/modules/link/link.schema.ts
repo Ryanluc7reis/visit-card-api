449266922af1fe3c77c350d01b6fb53e78bf5f55
ryanluc7reis <ryan.dev18@outlook.com>
2024-09-21 01:42:39 -0300

    Adicionando validaçao de link para whatsapp e telegram

--- src/modules/link/link.schema.ts
+++ src/modules/link/link.schema.ts
@@ -8,18 +8,56 @@ export const createLinkSchema = Joi.object({
   links: Joi.array()
     .items(
       Joi.object({
-        url: Joi.string().required(),
         app: Joi.string().required(),
+        url: Joi.string()
+          .required()
+          .when("app", {
+            is: "Whatsapp",
+            then: Joi.string()
+              .pattern(/^https:\/\/wa\.me\/\d+$/)
+              .message(
+                'A URL do WhatsApp deve estar no formato: "https://wa.me/<numero>".'
+              ),
+            otherwise: Joi.string().when("app", {
+              is: "Telegram",
+              then: Joi.string()
+                .pattern(/^https:\/\/t\.me\/\+\d+$/)
+                .message(
+                  'A URL do Telegram deve estar no formato: "https://t.me/+<numero>".'
+                ),
+              otherwise: Joi.forbidden(),
+            }),
+          }),
       })
     )
     .required(),
 });
+
 export const editLinkSchema = Joi.object({
   id: objectId().required(),
   linkId: objectId().required(),
-  url: Joi.string().required(),
   app: Joi.string().required(),
+  url: Joi.string()
+    .required()
+    .when("app", {
+      is: "Whatsapp",
+      then: Joi.string()
+        .pattern(/^https:\/\/wa\.me\/\d+$/)
+        .message(
+          'A URL do WhatsApp deve estar no formato: "https://wa.me/<numero>".'
+        ),
+      otherwise: Joi.string().when("app", {
+        is: "Telegram",
+        then: Joi.string()
+          .pattern(/^https:\/\/t\.me\/\+\d+$/)
+          .message(
+            'A URL do Telegram deve estar no formato: "https://t.me/+<numero>".'
+          ),
+        otherwise: Joi.forbidden(),
+      }),
+    }),
 });
+
 export const deleteLinkSchema = Joi.object({
   id: objectId().required(),
   linkId: objectId().required(),
