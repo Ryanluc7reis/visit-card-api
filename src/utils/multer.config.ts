4ef744447ebdc5732cc29eff8bdfdcfc0ca66c08
ryanluc7reis <ryan.dev18@outlook.com>
2024-09-25 17:32:02 -0300

    Instalando e configurando multer

---
+++ src/utils/multer.config.ts
@@ -0,0 +1,31 @@
+import multer, { DiskStorageOptions, FileFilterCallback } from "multer";
+import path from "path";
+import fs from "fs";
+import { Request } from "express";
+
+type Callback = (error: Error | null, destination: string) => void;
+
+export const storage = multer.diskStorage({
+  destination: (
+    req: Request,
+    file: Express.Multer.File,
+    callback: Callback
+  ): void => {
+    const uploadDir = "uploadsImages";
+
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir, { recursive: true });
+    }
+
+    callback(null, uploadDir);
+  },
+  filename: (
+    req: Request,
+    file: Express.Multer.File,
+    callback: Callback
+  ): void => {
+    const time = new Date().getTime();
+    const extName = path.extname(file.originalname);
+    callback(null, `${time}${extName}`);
+  },
+});
