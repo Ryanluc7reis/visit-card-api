2acded53759e2ecbcf63a5aabd608c7e03050db6
ryanluc7reis <ryan.dev18@outlook.com>
2024-09-27 18:37:31 -0300

    Ajustando multer config

--- src/utils/multer.config.ts
+++ src/utils/multer.config.ts
@@ -5,27 +5,5 @@ import { Request } from "express";
 
 type Callback = (error: Error | null, destination: string) => void;
 
-export const storage = multer.diskStorage({
-  destination: (
-    req: Request,
-    file: Express.Multer.File,
-    callback: Callback
-  ): void => {
-    const uploadDir = "src/uploadsImages";
-
-    if (!fs.existsSync(uploadDir)) {
-      fs.mkdirSync(uploadDir, { recursive: true });
-    }
-
-    callback(null, uploadDir);
-  },
-  filename: (
-    req: Request,
-    file: Express.Multer.File,
-    callback: Callback
-  ): void => {
-    const time = new Date().getTime();
-    const extName = path.extname(file.originalname);
-    callback(null, `${time}${extName}`);
-  },
-});
+export const storage = multer.memoryStorage();
+export const upload = multer({ storage });
